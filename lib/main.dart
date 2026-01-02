aad8f8ea6fcc51952be005486ebf032e596cbfda
Typezera <willianbardela2131@hotmail.com>
2026-01-02 19:19:10 -0300

    FEAT: adicionando um IconButton no app bar e um floatingActionButton na parte inferior para melhor acessibilidade.

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add)
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -41,6 +47,11 @@ class MyHomePage extends StatelessWidget {
           ]
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
